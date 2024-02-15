7bf92e69f8c9111a0c582ff4f764fa2e1bd23097
Wen <wenxiuchun@gmail.com>
2024-02-15 12:59:50 +0800

    Initial commit

---
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MyLibService} from './my-lib.service';
+
+@Component({
+  selector: 'lib-my-lib',
+  template: `
+    <p>
+      my-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyLibComponent {
+  constructor(myLibService: MyLibService) {
+    myLibService.getHeros();
+  }
+}
---
+++ projects/my-lib/src/lib/my-lib.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MyDecorator } from './my-decorator';
+@MyDecorator
+@Injectable({
+  providedIn: 'root'
+})
+export class MyLibService {
+
+  constructor() {
+    console.log("old Constructor")
+  }
+
+  getHeros() {
+    console.log("my lib service get heros");
+  }
+}
